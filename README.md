171d09f5b77a0d419196c0e1267e252523cdc246
Gevorg Aznauryan <gazilius@gmail.com>
2017-06-29 02:24:58 +0300

    ntly

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ns-cell
-# ns-hive
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 /**
  * Created by GAZ on 6/28/17.
  */
+require('dotenv').config({path: __dirname+'/.env'});
+var pj = require('./package.json');
+var os = require('os');
+var options = {
+    endpoint: process.env.NS_MONITOR_ENDPOINT,
+    namespace: process.env.NS_MONITOR_NAMESPACE,
+    meta: {
+        node: 'ns-cell',
+        version: pj.version,
+        os:{
+            hostname: os.hostname(),
+            pid: process.pid
+        },
+
+    }
+};
+
+var client = require('@netshards/ns-monitor').client(options);
